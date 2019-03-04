7e815f22653b07b4f82a4f8f405f3cb57df2e6d3
Slavko Kolodiy <spkolodiy@gmail.com>
2019-03-04 20:21:49 +0200

    fixed random map generation and improved main

--- GameHal/GameHal/main.cpp
+++ GameHal/GameHal/main.cpp
@@ -1,11 +1,41 @@
 #include"map.h"
 #include<ctime>
+#include<fstream>
 
 int main()
 {
 	srand(time(NULL));
 	Map m;
-	m.generateRandomMap("Read_Edit_map.txt", 7, 7);
+	ofstream delCont;
+	m.generateRandomMap("Read_Edit_Map.txt", 5, 5);
+	while (true)
+	{
+		int action;
+		cout << "what do u want to do?(press 1 to save your changes made to map, press 2 to generate new random map, 3 to exit) " << endl;
+		cin >> action;
+		switch (action)
+		{
+		case 1:
+			m.readMapFromFile("Read_Edit_Map.txt");
+			cout << "changes saved" << endl;
+			break;
+		case 2:
+			cout << "enter size" << endl;
+			int height, length;
+			cin >> height >> length;
+			delCont.open("Read_Edit_Map.txt", ofstream::out | ofstream::trunc);
+			delCont.close();
+			m.generateRandomMap("Read_Edit_Map.txt", height, length);
+			cout << "new map was generated" << endl;
+			break;
+		default:
+			break;
+		}
+		if (action == 3)
+		{
+			break;
+		}
+	}
 	system("pause");
 	return 0;
 }
