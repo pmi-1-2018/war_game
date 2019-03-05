a8b00ad63f37cb2f0eca85ffd55131ee8e530fc2
Volodymyr Andryushko <volodymyr.andryushko@gmail.com>
2019-03-05 14:45:04 +0200

    added print func

--- GameHal/GameHal/Map.h
+++ GameHal/GameHal/Map.h
@@ -23,5 +23,38 @@ public:
 	void setWidth(int width);
 	void setHeight(int height);
 	~Map();
+	friend ostream& operator<<(ostream& sout, Map m)
+	{
+		for (int i = 0; i < m.getWidth(); i++)
+		{
+			sout << '_';
+		}
+		sout << endl;
+		for (int i = 0; i < m.getHeight(); i++)
+		{
+			for (int j = 0; j < m.getWidh(); j++)
+			{
+				if (m.map[i][j].getPassCost() == 1)
+				{
+					sout << ' ';
+				}
+				else if (m.map[i][j].getPassCost() == 0)
+				{
+					sout << "#";
+				}
+				else
+				{
+					sout << m.map[i][j].getPassCost();
+				}
+			}
+			sout << '|' << endl;
+		}
+		for (int i = 0; i < m.getWidth(); i++)
+		{
+			sout << '-';
+		}
+		sout << endl;
+		return sout;
+	}
 };
 
