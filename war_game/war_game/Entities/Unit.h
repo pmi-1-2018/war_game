8d0129146f71e00b120019db15cf407e85cd0461
Diana Hanushkevych <dganushkevych@gmail.com>
2019-03-12 13:29:01 +0200

    Unit.h

---
+++ war_game/war_game/Entities/Unit.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Unit
+{
+protected:
+	int damage = -1;
+	int defense = -1;
+	int healthPoints = -1;
+public:
+	int GetDefense() { return defense; }
+	int GetDamage() { return damage; }
+	int GetHealthPoints() { return healthPoints; }
+	virtual int DamageTaken() { return 0; }
+
+	void SetDamage(int new_damage) { damage = new_damage; }
+	void SetHealthPoints(int new_healthPoints) { healthPoints = new_healthPoints; }
+
+	virtual void attack(Unit& unit) {};
+
+};
