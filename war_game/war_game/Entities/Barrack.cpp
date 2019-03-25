da0da11cff7dbb2e22b5422ada0d86a641169c7a
S1NED <denic.poruchnik@gmail.com>
2019-03-25 20:58:28 +0200

    modified Barrack.h and Barrack.cpp

--- war_game/war_game/Entities/Barrack.cpp
+++ war_game/war_game/Entities/Barrack.cpp
@@ -1,3 +1,122 @@
 #include"Barrack.h"
 
+int Barrack::GetNumberOfSwordsmen()
+{
+	return numberOfSwordsmen;
+}
 
+void Barrack::SetNumberOfSwordsmen(int number)
+{
+	this->numberOfSwordsmen = number;
+}
+
+int Barrack::GetNumberOfArchers()
+{
+	return numberOfArchers;
+}
+
+void Barrack::SetNumberOfArchers(int number)
+{
+	this->numberOfArchers = number;
+}
+
+int Barrack::GetNumberOfTanks()
+{
+	return numberOfTanks;
+}
+
+void Barrack::SetNumberOfTanks(int number)
+{
+	this->numberOfTanks = number;
+}
+
+int Barrack::GetNumberOfWizards()
+{
+	return numberOfWizards;
+}
+
+void Barrack::SetNumberOfWizards(int number)
+{
+	this->numberOfWizards = number;
+}
+
+void Barrack::giveUnits(Army army)
+{
+	int numberOfUnits;
+	char letter;
+	cout << "enter which units you want to take: ";
+	switch (letter)
+	{
+	case 'S':
+		cout << "enter how many swordsmen you want to take: ";
+		cin >> numberOfUnits;
+		if (numberOfUnits <= numberOfSwordsmen)
+		{
+			for (int i = 0; i < numberOfUnits; i++)
+			{
+				army.addUnit(Swordsman());
+				army.SetCurrEnergy(-1);
+			}
+		}
+		break;
+
+	case 'A' :
+		cout << "enter how many archers you want to take: ";
+		cin >> numberOfUnits;
+		if (numberOfUnits <= numberOfArchers)
+		{
+			for (int i = 0; i < numberOfUnits; i++)
+			{
+				army.addUnit(Archer());
+				army.SetCurrEnergy(-1);
+			}
+		}
+		break;
+	case 'T' :
+		cout << "enter how many tancs you want to take: ";
+		cin >> numberOfUnits;
+		if (numberOfUnits <= numberOfTanks)
+		{
+			for (int i = 0; i < numberOfUnits; i++)
+			{
+				army.addUnit(Tank());
+				army.SetCurrEnergy(-1);
+			}
+		}
+		break;
+	case 'W' :
+		cout << "enter how many wizards you want to take: ";
+		cin >> numberOfUnits;
+		if (numberOfUnits <= numberOfWizards)
+		{
+			for (int i = 0; i < numberOfUnits; i++)
+			{
+				army.addUnit(Wizard());
+				army.SetCurrEnergy(-1);
+			}
+		}
+		break;
+	}
+}
+
+int Barrack::GetNumberOfTurn()
+{
+	return numberOfTurn;
+}
+
+
+void Barrack::SetNumberOfTurn(int number)
+{
+	this->numberOfTurn = number;
+}
+
+/*void Barrack::renewal()
+{
+	if (turnnum - GetNumberOfTurn() >= 5 && turnnum - GetNumberOfTurn() <= 10)
+	{
+		SetNumberOfSwordsmen(GetNumberOfSwordsmen() + 5);
+		SetNumberOfArchers(GetNumberOfArchers() + 3);
+		SetNumberOfTanks(GetNumberOfTanks() + 1);
+		SetNumberOfWizards(GetNumberOfWizards() + 2);
+	}
+}*/
--- war_game/war_game/Entities/Barrack.h
+++ war_game/war_game/Entities/Barrack.h
@@ -1,7 +1,33 @@
 #pragma once
+#include"Army.h"
+#include"Map.h"
+#include "Swordsman.h"
+#include "Tank.h"
+#include "Wizard.h"
 
 class Barrack
 {
+protected:
+	int numberOfSwordsmen;
+	int numberOfArchers;
+	int numberOfTanks;
+	int numberOfWizards;
+	int numberOfTurn;
+public:
+	int GetNumberOfSwordsmen();
+	void SetNumberOfSwordsmen(int number);
+	int GetNumberOfArchers();
+	void SetNumberOfArchers(int number);
+	int GetNumberOfTanks();
+	void SetNumberOfTanks(int number);
+	int GetNumberOfWizards();
+	void SetNumberOfWizards(int number);
+	
+	int GetNumberOfTurn();
+	void SetNumberOfTurn(int number);
 
+
+	void giveUnits(Army army);
+	void renewal();
 };
 
