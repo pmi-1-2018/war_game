11424004de670a4cf9466d15396f88f9b6c6f37c
InQueueq <dimadorko@gmail.com>
2019-04-23 14:33:18 +0300

    fixed exceptions

--- war_game/war_game/Entities/Healer.cpp
+++ war_game/war_game/Entities/Healer.cpp
@@ -2,9 +2,10 @@
 
 void Healer::Heal(Unit& other_unit)
 {
+	
 	if (other_unit.GetHealthPoints() < other_unit.GetMaxHP())
 	{
-		other_unit.SetHealthPoints(other_unit.GetHealthPoints() + 30);
+		other_unit.SetHealthPoints(other_unit.GetHealthPoints() + 10);
 		if (other_unit.GetHealthPoints() > other_unit.GetMaxHP())
 		{
 			other_unit.SetHealthPoints(other_unit.GetMaxHP());
